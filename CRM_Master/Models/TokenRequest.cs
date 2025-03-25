92ed5633be07f59af1dc6b0a051023f6c4807863
ainamanampisoa <169085336+ainamanampisoa@users.noreply.github.com>
2025-03-25 13:26:18 +0300

    Commit done 2615
    -login avec exisitingApp

---
+++ CRM_Master/Models/TokenRequest.cs
@@ -0,0 +1,8 @@
+// Models/LoginModel.cs
+namespace CRM_Master.Models.request
+{
+    public class TokenRequest
+    {
+        public string Token { get; set; }
+    }
+}
--- CRM_Master/obj/Debug/net9.0/CRM_Master.AssemblyInfo.cs
+++ CRM_Master/obj/Debug/net9.0/CRM_Master.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CRM_Master")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce57d6344be28843900ccc1a6aec3c988209562e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef202d1bc285227d080259fbf22377019ca2ebce")]
 [assembly: System.Reflection.AssemblyProductAttribute("CRM_Master")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CRM_Master")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
